b9c268fc9d67c7ef80d1410a80c87dc2618020cd
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-06-13 14:16:15 -0400

    MVP for spring & (starting Understanding phase of stretch problems  using UPER)

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -93,9 +93,50 @@ class SortingRobot:
         return self._light == "ON"
 
 
+    def sort(self):
+        self.set_light_on()
+        # While we still have items to sort
+        while self.light_is_on():
+            # Turn light off - if we don't swap - it leaves the loop to signal completion
+            self.set_light_off()
+            # while positions are available to the right - we can continue sorting
+            while self.can_move_right():
+                    # Get item at cur pos and go right to start the comparison
+                self.swap_item()
+                self.move_right()
+
+                # If our item is bigger than shelf item
+                if self.compare_item() == 1:
+                    # swap, move left, (swap - smaller item on shelf), move right, turn light on to signal SWAP
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_on()
+                else:
+                    # Our item is less than the cur pos, move left, swap, move right
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                # Reached end of list - go back to pos 0
+            while self.can_move_left():
+                self.move_left()
+        
+        
 
 
-"""
+if __name__ == "__main__":
+    # Test our your implementation from the command line
+    # with `python robot_sort.py`
+
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
+    robot = SortingRobot(l)
+
+    robot.sort()
+    print(robot._list)
+
+    """
         Sort the robot's list.
         This Robot moves either left or right - This is a clue to use BUBBLE SORT
         The light can be used to notify when we SWAP, so we can keep looping like a bubble sort pattern. 
@@ -121,37 +162,3 @@ class SortingRobot:
                     go back to prev position - drop our item - and move right again
             set our robot to pos 0 - by moving left until we can't move left anymore
         """
-    def sort(self):
-        self.set_light_on()
-        # While we still have items to sort
-        while self.light_is_on():
-            # Turn light off - if we don't swap - it leaves the loop to signal completion
-            self.set_light_off():
-            # while positions are available to the right - we can continue sorting
-            while self.can_move_right():
-                # Get item at cur pos and go right to start the comparison
-                self.swap_item()
-                self.move_right()
-            
-            # If out item is bigger than the self item
-            if self.compare_item() == 1:
-                # swap, move left, (swap - smaller item on shelf), move right, turn light on to signal SWAP
-                self.swap_item()
-                self.move_left()
-                self.swap_item()
-                self.move_right()
-                self.light_is_on()
-        
-        
-
-
-if __name__ == "__main__":
-    # Test our your implementation from the command line
-    # with `python robot_sort.py`
-
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
-    robot = SortingRobot(l)
-
-    robot.sort()
-    print(robot._list)
